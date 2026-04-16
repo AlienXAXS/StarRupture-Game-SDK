d5255faa4c5a6a19c054e414e7d37e8709bfa9e2
AlienXAXS <github@smtpd.co.uk>
2026-04-16 15:23:21 +0100

    Remove problematic assert

--- Server/SDK/MassAIPrototypeEnemyRuntime_structs.hpp
+++ Server/SDK/MassAIPrototypeEnemyRuntime_structs.hpp
@@ -2616,7 +2616,7 @@ public:
 	uint8                                         Pad_C[0x4];                                        // 0x000C(0x0004)(Fixing Size After Last Property [ Dumper-7 ])
 	TSubclassOf<class UGameplayEffect>            AttackDamageEffect;                                // 0x0010(0x0008)(Edit, ZeroConstructor, NoDestructor, UObjectWrapper, HasGetValueTypeHash, NativeAccessSpecifierPublic)
 };
-DUMPER7_ASSERTS_FAttackAggroTargetTaskInstanceData;
+//DUMPER7_ASSERTS_FAttackAggroTargetTaskInstanceData;
 
 // ScriptStruct MassAIPrototypeEnemyRuntime.MassEnemyFollowActorTaskInstanceData
 // 0x0000 (0x0001 - 0x0001)
